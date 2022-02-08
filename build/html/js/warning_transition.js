b3ee0658444acd682891daa726f39bb0bb621922
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2022-02-07 22:09:03 -0500

    Organized file system and added 404 page

---
+++ build/html/js/warning_transition.js
@@ -0,0 +1,12 @@
+var cover_opacity = 0;
+
+function transitionToMain() {
+  if (cover_opacity < 1) {
+    document.getElementById("cover").style.zIndex = "1000";
+    cover_opacity += .075;
+    setTimeout(function() {
+      transitionToMain()
+    }, 75);
+  }
+  document.getElementById('cover').style.opacity = cover_opacity;
+}
