e9629094689964c87f78da4abb0a3823184dee89
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2022-02-09 22:08:21 -0500

    Fixed main page transition error

--- src/js/main_page.js
+++ src/js/main_page.js
@@ -1,14 +1,14 @@
-var cover_opacity = 0;
+var cover_opacity = 1;
 function onOpen() {
-  if (cover_opacity < 1) {
-    cover_opacity += .05;
+  if (cover_opacity > 0) {
+    cover_opacity -= .05;
     setTimeout(function() {
       onOpen()
     }, 75);
   }
   document.getElementById('cover').style.opacity = cover_opacity;
-  if (cover_opacity > 1) {
-    document.getElementById("cover").style.zIndex = "-1";
+  if (cover_opacity < 0) {
+    document.getElementById("cover").style.zIndex = "-1000";
   }
 }
 
