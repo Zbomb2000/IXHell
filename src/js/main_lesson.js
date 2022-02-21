bb0c369d7c191ba1047e4cf61b8aa2bc6a927df8
ZBOMB <75494994+Zbomb2000@users.noreply.github.com>
2022-02-20 22:40:27 -0500

    I'm an idiot
    I got the testJsonStuff() function to work because I was referencing the wrong variable.

--- src/js/main_lesson.js
+++ src/js/main_lesson.js
@@ -1,6 +1,6 @@
 // Sends console messages
 console.log("I recommend not using the console to cheat.");
-console.log("It's not very hard, but it's also not fun.");
+console.log("It makes the program not fun.");
 console.log("That's pretty much it.");
 
 // THIS IS REALLY IMPORTANT. IT LOADS THE LESSON DATA FROM THE JSON FILE
@@ -11,18 +11,17 @@ var display_text = "";
 
 function testJsonStuff(ld) {
   // Gets data
-  var q1 = ld["qone"];
+  var q1 = ld["q_one"];
 
   // Gets important number
-  if (ld[1] == true) {
+  if (q1[1] == true) {
     display_text = q1[3];
-  }
-  else {
+  } else {
     display_text = q1[2];
   }
 
   // Displays question
-  document.getElementById("question-box").innerHTML = q1[display_text];
+  document.getElementById("question-box").innerHTML = display_text;
 }
 
 testJsonStuff(lesson_data);
