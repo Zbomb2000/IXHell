16ae0b512cf9c4120cfa5e4a8af9330ce71cd6fc
Zbomb2000 <hxl9500@gmail.com>
2022-03-10 14:31:14 -0500

    Added basic random questions and answer check function

--- src/js/main_lesson.js
+++ src/js/main_lesson.js
@@ -6,25 +6,39 @@
 */
 
 console.log("I recommend not using the console to cheat.");
-console.log("It makes the program not fun.");
+console.log("It makes it not fun.");
 console.log("That's pretty much it.");
 
-// Load json
-var json_lesson_data = window.lesson_data;
+var answer_var;
 
-function setQuestion(ld) {
-  var display_text = "";
+function setQuestion() {
+  var num1 = Math.floor(Math.random() * 100);
+  var num2 = Math.floor(Math.random() * 100);
+  var answer = num1 + num2;
 
-  // Gets data from json dictionary
-  var question_info = ld["q_one"];
-
-  if (question_info[1] == true) {
-    display_text = question_info[3];
-  } else {
-    display_text = question_info[2];
-  }
-
-  document.getElementById("question-box").innerHTML = display_text;
+  document.getElementById("num1").innerHTML = num1;
+  document.getElementById("num2").innerHTML = num2;
+  document.getElementById("answer").innerHTML = answer;
 }
 
-setQuestion(lesson_data);
+function submitAnswer() {
+  var user_answer = document.getElementById("answer-input").value;
+  if (user_answer == document.getElementById("answer").innerHTML) {
+    console.log("Correct!");
+  }
+  else {
+    console.log("You're an idiot.");
+  }
+}
+
+// Click submit when enter is clicked
+var input = document.getElementById("answer-input");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("submit").click();
+  }
+});
+
+
+setQuestion();
