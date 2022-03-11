b40b415b47f1b00547c4a0877b27b412cf1a2d9a
Zbomb2000 <hxl9500@gmail.com>
2022-03-11 12:13:57 -0500

    Added files to build

--- build/README.md
+++ build/README.md
@@ -1,2 +1,2 @@
-# How to Run the Program
-I'll add stuff here later
+## FOLDER INFO
+This folder is the part of the repo that I'm writing to when changes are made. The build folder is for the final product. This is for working on the product.
---
+++ build/js/main_page.js
@@ -0,0 +1,15 @@
+var cover_opacity = 1;
+function onOpen() {
+  if (cover_opacity > 0) {
+    cover_opacity -= .05;
+    setTimeout(function() {
+      onOpen()
+    }, 75);
+  }
+  document.getElementById('cover').style.opacity = cover_opacity;
+  if (cover_opacity < 0) {
+    document.getElementById("cover").style.zIndex = "-1000";
+  }
+}
+
+onOpen();
